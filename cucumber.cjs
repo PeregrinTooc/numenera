fb21a9baf60a82a8e84f7f8c64e86fa00e20721c
Philipp Rembold <philipp.rembold@sap.com>
2026-01-24 23:10:27 +0100

    tweak cucumber settings

--- cucumber.cjs
+++ cucumber.cjs
@@ -5,9 +5,9 @@ module.exports = {
     format: ["progress", "html:test-results/cucumber-report.html"],
     formatOptions: { snippetInterface: "async-await" },
     tags: "not @skip and not @wip",
-    timeout: 30000, // 30 seconds for e2e tests with modal rendering
+    timeout: 300, // 300 milliseconds for e2e tests with modal rendering
     failFast: true, // Stop on first failure
     // eslint-disable-next-line no-undef
-    parallel: process.env.CI ? 1 : 4, // 4 workers locally
+    parallel: process.env.CI ? 1 : 6, // 6 workers locally
   },
 };
